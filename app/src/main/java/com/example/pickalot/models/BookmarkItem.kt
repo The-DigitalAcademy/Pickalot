ff4794aa0f6181c8ba1dfcde45c99f04a2526dd8
MononoMorapeli <108496671+mononomorapeli@users.noreply.github.com>
2023-07-04 14:53:58 +0200

    bookmark model added

--- app/src/main/java/com/example/pickalot/models/BookmarkItem.kt
+++ app/src/main/java/com/example/pickalot/models/BookmarkItem.kt
@@ -11,8 +11,7 @@ data class BookmarkItem(
     @PrimaryKey(autoGenerate = true) val bookmarkId: Int? = null,
     val productId: Int? = null,
 
-    )
-{
+    ) {
     /** This will deals with the data from server and local */
     @Ignore
     var product: Product? = null
